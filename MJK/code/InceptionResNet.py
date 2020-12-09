bd1c1bae1b7db5e9ea0e0fd9450d263546da1248
MJK0211 <kimminjong0211@gmail.com>
2020-12-10 00:00:05 +0900

    12_10_commit

--- MJK/code/InceptionResNet.py
+++ MJK/code/InceptionResNet.py
@@ -17,9 +17,9 @@ train_datagen = ImageDataGenerator(rescale=1./255, #정규화
                                    vertical_flip=True, #수직
                                    width_shift_range=0.1, #수평이동
                                    height_shift_range=0.1, #수직이동
-                                   rotation_range=5,
+                                   rotation_range=10,
                                    zoom_range=1.2,
-                                   shear_range=0.7, #좌표보정, 좌표이동
+                                   shear_range=0.6, #좌표보정, 좌표이동
                                    fill_mode='nearest' #옮겼을때 빈자리를 그전과 비슷하게 채워준다
 )
 
@@ -30,7 +30,7 @@ pred_datagen = ImageDataGenerator(rescale=1./255.)
 xy_train = train_datagen.flow_from_directory(
     './MJK/data/train', #폴더 위치 
     target_size=(200,200), 
-    batch_size=19, 
+    batch_size=10, 
     class_mode='binary' 
     # save_to_dir='./data/img/data1_2/train' #전환된 이미지 데이터 파일을 이미지 파일로 저장
 ) 
@@ -38,7 +38,7 @@ xy_train = train_datagen.flow_from_directory(
 xy_test = test_datagen.flow_from_directory(
    './MJK/data/test',
     target_size=(200,200),
-    batch_size=4,  
+    batch_size=3,  
     class_mode='binary'
     # save_to_dir='./data/img/data1_2/test'
 )
@@ -60,37 +60,30 @@ inceptionResNetV2.trainable = False
 
 model = Sequential()
 model.add(inceptionResNetV2)
-model.add(Flatten())
-model.add(Dense(100, activation = 'relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
-
-# Layer (type)                 Output Shape              Param #
-# =================================================================
-# inception_resnet_v2 (Functio (None, 4, 4, 1536)        54336736
-# _________________________________________________________________
-# flatten (Flatten)            (None, 24576)             0
-# _________________________________________________________________
-# dense (Dense)                (None, 100)               2457700
-# _________________________________________________________________
-# dense_1 (Dense)              (None, 1)                 101
-# =================================================================
-# Total params: 56,794,537
-# Trainable params: 2,457,801
-# Non-trainable params: 54,336,736
-# _________________________________________________________________
-
+model.add(Flatten())   
+model.add(Dense(100, kernel_initializer='he_normal'))     
+model.add(BatchNormalization())
+model.add(Activation('relu')) 
+model.add(Dense(40, kernel_initializer='he_normal'))                                    
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(10, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics =['acc'])
-from tensorflow.keras.callbacks import EarlyStopping 
+modelpath = "D:/ImgDetection/MJK/data/weight/cp-{epoch:002d}-{val_loss: 4f}.hdf5"  
 
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics =['acc'])
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+early_stopping = EarlyStopping(monitor = 'val_loss', patience = 20)
+check_point = ModelCheckpoint(filepath = modelpath, monitor = 'val_loss', save_best_only = True, mode = 'val_loss')
 hist = model.fit_generator(
     xy_train, #train 안에 x, y의 데이터를 모두 가지고 있다
-    steps_per_epoch=20, #
-    epochs=30,
+    steps_per_epoch=38, #
+    epochs=100,
     validation_data=xy_test, #test도 x, y의 데이터를 모두 가지고 있다
-    validation_steps=3 #어떤건지 찾아보기
+    validation_steps=20, #어떤건지 찾아보기
+    callbacks=[early_stopping, check_point]
 )
 
 #4. 평가, 예측
@@ -98,18 +91,18 @@ loss, acc = model.evaluate(xy_test)
 print("loss : ", loss)
 print("acc : ", acc)
 
-y_pred = model.predict(predict)
-print("y_pred : ", y_pred.reshape(50,))
+y_pred = model.predict_generator(predict, steps=1, verbose=1)
+print("y_pred : ", y_pred)
 print(y_pred.shape)
 
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-rows = 5
-cols = 11
+rows = 7
+cols = 8
 
 # print(y_pred[1][0])
-a = ['SUJI', 'JOOHYUK']
+a = ['B', 'N']
 
 def printIndex(array, i):
     if np.round(array[i][0]) == 0:
@@ -126,3 +119,56 @@ for i in range(len(predict[0])):
     ax.set_xticks([]), ax.set_yticks([])
 plt.show()
 
+# loss :  0.14220841228961945
+# acc :  0.949999988079071
+# [[0.00511569]
+#  [0.7673361 ]
+#  [0.97431016]
+#  [0.01020213]
+#  [0.9927758 ]
+#  [0.7497511 ]
+#  [0.7221572 ]
+#  [0.99856645]
+#  [0.05838428]
+#  [0.66237336]
+#  [0.78285784]
+#  [0.9205434 ]
+#  [0.03582747]
+#  [0.07276288]
+#  [0.87418294]
+#  [0.99428517]
+#  [0.01810936]
+#  [0.9824897 ]
+#  [0.7433762 ]
+#  [0.37383893]
+#  [0.00172315]
+#  [0.83169407]
+#  [0.9973502 ]
+#  [0.6480843 ]
+#  [0.84695905]
+#  [0.00344487]
+#  [0.8763713 ]
+#  [0.9620815 ]
+#  [0.0057925 ]
+#  [0.01655022]
+#  [0.9447976 ]
+#  [0.02900423]
+#  [0.9586486 ]
+#  [0.01404348]
+#  [0.01092414]
+#  [0.99086505]
+#  [0.05920314]
+#  [0.9963148 ]
+#  [0.05133378]
+#  [0.97355723]
+#  [0.4944659 ]
+#  [0.74268365]
+#  [0.00289558]
+#  [0.05846003]
+#  [0.99600464]
+#  [0.9975501 ]
+#  [0.9244332 ]
+#  [0.99559987]
+#  [0.5892266 ]
+#  [0.97763604]]
+# (50, 1)
--- MJK/opencv_test.py
+++ MJK/opencv_test.py
@@ -1,29 +1,146 @@
 import cv2
-vidcap = cv2.VideoCapture('./Common_data/avi/startup_13.mp4')
-success, image = vidcap.read()
-
-# count = 1
 
 
-# while (vidcap.isOpened()):
-#   success, image = vidcap.read()
-# #   cv2.imwrite("./Common_data/photo/%d.jpg" %count, image)
-# #   print("saved image %d.jpg" % count)
+xml_path1 = "C:\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml"
+xml_path2 = "C:\Anaconda3\Lib\site-packages\cv2\data\haarcascade_profileface.xml"
+xml_path3 = "C:\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml"
+xml_path4 = "C:\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt_tree.xml"
+xml_path5 = "C:\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt2.xml"
 
 
-#   print(image)
-#   if cv2.waitKey(10) == 27:                    
-#       break
-#   count += 1
-while (vidcap.isOpened()):
-  ret, frame = vidcap.read()
+face_classifier  = cv2.CascadeClassifier(xml_path1)
+face_classifier2 = cv2.CascadeClassifier(xml_path2)
+face_classifier3 = cv2.CascadeClassifier(xml_path3)
+face_classifier4 = cv2.CascadeClassifier(xml_path4)
+face_classifier5 = cv2.CascadeClassifier(xml_path5)
 
-  if ret:
-        cv2.imshow('video', frame)
-        if cv2.waitKey(10) == 27:                    
-            break
-  else:
-      break
+def face_detector(img, size = 0.5):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+    if faces is():
+        faces = face_classifier2.detectMultiScale(gray,1.3,5)
+        if faces is():
+            faces = face_classifier3.detectMultiScale(gray,1.3,5)
+            if faces is():
+                faces = face_classifier4.detectMultiScale(gray,1.3,5)
+                if faces is():
+                    faces = face_classifier5.detectMultiScale(gray,1.3,5)
+                    if faces is():
+                        return img,[] 
+                      
+    rr = []    
+    for(x,y,w,h) in faces:
+        try:  
+            print(faces[0])  
+            print("111111111111",faces[1])  
+            if faces[1] is not None:
+                print("ifif")
+                for j in range(len(faces)):
+                    print("forfor")
+                    print(j)
+                    for(x,y,w,h) in [faces[j]]:
+                        print("x,y,w,h",x,y,w,h)
+                        print("forforforfor")
+                        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
+                        roi = img[y:y+h, x:x+w]
+                        roi = cv2.resize(roi, (200,200))
+                        rr = rr.append(roi)
+                        print("rrrr",rr)
+                return img,rr    
+        except:
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
+            roi = img[y:y+h, x:x+w]
+            roi = cv2.resize(roi, (200,200))
+            return img,roi   #검출된 좌표에 사각 박스 그리고(img), 검출된 부위를 잘라(roi) 전달
 
+    
+
+# ==================================================================================
+
+#Open the File
+vidcap = cv2.VideoCapture('./Common_data/avi/startup_13.mp4') 
+
+#Check that the file is opened
+if vidcap.isOpened() == False: #동영상 핸들 확인
+    print('Can\'t open the File' + (FilePath))
+    exit()
+
+#create the window & change the window size
+#윈도우 생성 및 사이즈 변경
+# cv2.namedWindow('Face')
+
+
+while True:
+    #카메라로 부터 사진 한장 읽기 
+    ret, frame = vidcap.read()
+
+    # 얼굴 검출 시도 
+    image, face = face_detector(frame)
+    try:
+        if face[1] is not None:
+            face = np.array(face)
+    except:
+        pass
+    Training_Data = []
+    try:
+        #검출된 사진을 흑백으로 변환 
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        Training_Data.append(np.asarray(face, dtype=np.uint8))
+        face = np.array(Training_Data)
+        face = face.reshape(face.shape[0],100,100,4)
+
+        #위에서 학습한 모델로 예측시도
+        result = model.predict(face)
+        # result = np.argmax(result)
+        #result[1]은 신뢰도이고 0에 가까울수록 자신과 같다는 뜻이다. 
+        # if result[1] < 500:
+        #     #????? 어쨋든 0~100표시하려고 한듯 
+        #     confidence = int(100*(1-(result[1])/300))
+        #     # 유사도 화면에 표시 
+        #     display_string = str(confidence)+'% Confidence it is user'
+
+        # cv2.putText(image,display_string,(100,120), cv2.FONT_HERSHEY_COMPLEX,1,(250,120,255),2)
+        #75 보다 크면 동일 인물로 간주해 UnLocked! 
+        try: 
+            if result[1] is not None:
+                print("if")
+                for i in range(len(result)):
+                    print("for")
+                    if result[i] >= 0.0 and result[i] < 0.02:
+                        print(result)
+                        cv2.putText(image, "B", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                        cv2.imshow('Face Cropper', image)
+                    elif result[i] >= 0.98 and result[i] <= 1.02:
+                        print(result)
+                        cv2.putText(image, "N", (300, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                        cv2.imshow('Face Cropper', image)
+                    else:
+                        print(result)
+                        #75 이하면 타인.. Locked!!! 
+                        cv2.putText(image, "U", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                        cv2.imshow('Face Cropper', image)
+        except:
+            pass
+        if result >= 0.0 and result < 0.02:
+            print(result)
+            cv2.putText(image, "B", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Face Cropper', image)
+        elif result >= 0.98 and result <= 1.02:
+            print(result)
+            cv2.putText(image, "N", (300, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Face Cropper', image)
+        else:
+            print(result)
+            #75 이하면 타인.. Locked!!! 
+            cv2.putText(image, "U", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            cv2.imshow('Face Cropper', image)
+    except:
+        #얼굴 검출 안됨 
+        cv2.putText(image, "Face Not Found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+        cv2.imshow('Face Cropper', image)
+        pass
+
+    if cv2.waitKey(5)==13:
+        break
 vidcap.release()
 cv2.destroyAllWindows()
